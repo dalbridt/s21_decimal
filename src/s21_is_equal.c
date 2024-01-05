4a5988560439cf92f7815c892673d63a930e7b17
avrbv <81697398+avrbv@users.noreply.github.com>
2024-01-05 19:34:51 +0300

    files structure created

---
+++ src/s21_is_equal.c
@@ -0,0 +1,6 @@
+#include "s21_decimal.h"
+
+int s21_is_equal(s21_decimal value_1, s21_decimal value_2) {
+  //
+  return 0;
+}
---
+++ src/s21_is_less.c
@@ -0,0 +1,6 @@
+#include "s21_decimal.h"
+
+int s21_is_less(s21_decimal value_1, s21_decimal value_2) {
+  //
+  return 0;
+}
