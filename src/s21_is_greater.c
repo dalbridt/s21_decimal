fa9bb8f62a334724db5f0ed1d5e949c4b4ad4a20
avrbv <81697398+avrbv@users.noreply.github.com>
2024-01-27 12:41:09 +0300

    Update s21_is_greater.c

--- src/s21_is_greater.c
+++ src/s21_is_greater.c
@@ -4,15 +4,17 @@ int s21_is_greater(s21_decimal value_1, s21_decimal value_2) {
   bool value;
   int sign_1 = get_sign(value_1);
   int sign_2 = get_sign(value_2);
-  if (sign_1 > sign_2) {
-    value = false;
-  } else if (sign_1 < sign_2) {
-    value = true;
+  if (sign_1 != sign_2) {
+    value = sign_1 > sign_2;
   } else {
     int exp_1 = get_scale(value_1);
     int exp_2 = get_scale(value_2);
     if (exp_1 != exp_2) {
-      equalize_scale(&value_1, &value_2);
+      if (exp_1 < exp_2) {
+        equalize_scale(&value_1, exp_2);
+      } else {
+        equalize_scale(&value_2, exp_1);
+      }
     }
     if (sign_1 == 1) {
       value = get_mantissa(&value_1) < get_mantissa(&value_2);
