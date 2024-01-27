bca70185b19581ca8ddb08603930e452a9789b69
dalbridt_home <anastasia@MacBook-Air-Anastasia.local>
2024-01-27 12:47:02 +0300

    upd is_greater

--- src/s21_is_greater.c
+++ src/s21_is_greater.c
@@ -5,16 +5,12 @@ int s21_is_greater(s21_decimal value_1, s21_decimal value_2) {
   int sign_1 = get_sign(value_1);
   int sign_2 = get_sign(value_2);
   if (sign_1 != sign_2) {
-    value = sign_1 > sign_2;
+    value = sign_1 < sign_2;
   } else {
     int exp_1 = get_scale(value_1);
     int exp_2 = get_scale(value_2);
     if (exp_1 != exp_2) {
-      if (exp_1 < exp_2) {
-        equalize_scale(&value_1, exp_2);
-      } else {
-        equalize_scale(&value_2, exp_1);
-      }
+      equalize_scale(&value_1, &value_2);
     }
     if (sign_1 == 1) {
       value = get_mantissa(&value_1) < get_mantissa(&value_2);
