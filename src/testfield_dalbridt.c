c702cf2449d702a29cff8f37bc6b83c00e30f50b
dalbridt_home <anastasia@MacBook-Air-Anastasia.local>
2024-02-01 21:41:37 +0300

    ручные тесты конверсия биг децимал

--- src/testfield_dalbridt.c
+++ src/testfield_dalbridt.c
@@ -10,41 +10,44 @@ float rand_float(int random, float min, float max) {
 }
 
 int main() {
-  // int fail = 0;
-  // for (int i = 0; i < 1000000; i++) {
-  //   float fl = rand_float(i, -F_MAX, F_MAX);
-  //   float fl_r = floor(fl);
-  //   s21_decimal dec, dec_rnd, dec_res;
-  //   s21_from_float_to_decimal(fl, &dec);
-  //   s21_from_float_to_decimal(fl_r, &dec_rnd);
-  //   s21_floor(dec, &dec_res);
-
-  //   if (s21_is_not_equal(dec_rnd, dec_res)) {
-  //     fail++;
-  //     printf(" orig float:  %f | build-in round: %f  \n", fl, fl_r);
-  //     debug_display_decimal("dec round", dec_res);
-  //   }
-  // }
-  // printf(("\nfails: %d\n"), fail);
   int fail = 0;
-  for (int i = 0; i < 100; i++) {
-    float f1 = rand_float(i, -F_MAX, F_MAX);
-    float f2 = rand_float(i, -F_MAX, F_MAX);
-    float res = f1 * f2;
-    float conv_res;
-    s21_decimal dec1;
-    s21_decimal dec2;
-    s21_decimal dec_res;
-    s21_from_float_to_decimal(f1, &dec1);
-    s21_from_float_to_decimal(f2, &dec2);
-    s21_mul(dec1, dec2, &dec_res);
-    s21_from_decimal_to_float(dec_res, &conv_res);
-    if (fabs(conv_res - res) > 0.01) {
-      printf(" orig :  %f \n", res);
-      debug_display_decimal("", dec_res);
+  for (int i = 0; i < 10000000; i++) {
+    float fl = rand_float(i, -F_MAX, F_MAX);
+    float fl_res;
+    s21_decimal dec;
+    s21_big_decimal big_dec;
+    s21_from_float_to_decimal(fl, &dec);
+    // debug_display_decimal("before", dec);
+    import_to_big_decimal(dec, &big_dec);
+    reset_decimal(&dec);
+    import_to_small_decimal(big_dec, &dec);
+    s21_from_decimal_to_float(dec, &fl_res);
+    // debug_display_decimal("after", dec);
+    if (fabs(fl - fl_res) > 0.00001) {
+      printf("orig : %f | conversion res: %f\n", fl, fl_res);
       fail++;
     }
   }
-  printf(("\nfails: %d\n"), fail);
+  printf("big dec conversion fails: %d", fail);
+  // int fail = 0;
+  // for (int i = 0; i < 100; i++) {
+  //   float f1 = rand_float(i, -F_MAX, F_MAX);
+  //   float f2 = rand_float(i, -F_MAX, F_MAX);
+  //   float res = f1 * f2;
+  //   float conv_res;
+  //   s21_decimal dec1;
+  //   s21_decimal dec2;
+  //   s21_decimal dec_res;
+  //   s21_from_float_to_decimal(f1, &dec1);
+  //   s21_from_float_to_decimal(f2, &dec2);
+  //   s21_mul(dec1, dec2, &dec_res);
+  //   s21_from_decimal_to_float(dec_res, &conv_res);
+  //   if (fabs(conv_res - res) > 0.01) {
+  //     printf(" orig :  %f \n", res);
+  //     debug_display_decimal("", dec_res);
+  //     fail++;
+  //   }
+  // }
+  // printf(("\nfails: %d\n"), fail);
   return 0;
 }
