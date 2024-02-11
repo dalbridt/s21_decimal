11268ae9ed672542a0ac23caf048d85a43c871a1
avrbv <81697398+avrbv@users.noreply.github.com>
2024-02-11 22:15:14 +0300

    truncate fix, alltests=0

--- src/s21_truncate.c
+++ src/s21_truncate.c
@@ -3,20 +3,17 @@
 int s21_truncate(s21_decimal value, s21_decimal *result) {
   rnd_code flag = RND_ERR;
 
-  if (result != NULL) {
+  if (result != NULL && s21_decimal_validation(value)) {
     s21_reset(result);
     flag = RND_OK;
-    if (result != NULL && s21_decimal_validation(value)) {
-      int scale = s21_get_scale(value);
-      while (scale != 0) {
-        s21_div10(&value, false);
-        scale--;
-      }
-      s21_set_scale(&value, 0);
-      *result = value;
-    } else {
-      flag = RND_ERR;
+
+    int scale = s21_get_scale(value);
+    while (scale != 0) {
+      s21_div10(&value, false);
+      scale--;
     }
+    s21_set_scale(&value, 0);
+    *result = value;
   }
 
   return flag;
